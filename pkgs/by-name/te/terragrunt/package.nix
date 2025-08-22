8b8e5673ec16fb4adb9905ac9aec575fa9e32dee
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-08-22 22:44:18 +0200

    terragrunt: 0.85.0 -> 0.86.0

--- pkgs/by-name/te/terragrunt/package.nix
+++ pkgs/by-name/te/terragrunt/package.nix
@@ -1,9 +1,9 @@
-{
-  lib,
-  buildGo125Module,
-  fetchFromGitHub,
-  versionCheckHook,
-  mockgen,
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, versionCheckHook
+, mockgen
+, go_1_25
 }:
 buildGo125Module (finalAttrs: {
   pname = "terragrunt";
@@ -16,42 +16,38 @@ buildGo125Module (finalAttrs: {
     hash = "sha256-LJGc85oCDEW/z9I4Mcr9Pyv9MLqqN4Zu/nJK1LTD9mk=";
   };
 
-  nativeBuildInputs = [
-    mockgen
-  ];
+      nativeBuildInputs = [
+        versionCheckHook
+        mockgen
+      ];
 
-  proxyVendor = true;
-
-  preBuild = ''
+      preBuild = ''
     make generate-mocks
-  '';
+      '';
 
   vendorHash = "sha256-BXFtw7+f9Isnk6EB3U4eLlho5B3rTnofmWBDbbbroUs=";
 
-  doCheck = false;
+      doCheck = false;
 
-  ldflags = [
-    "-s"
-    "-X github.com/gruntwork-io/go-commons/version.Version=v${finalAttrs.version}"
-    "-extldflags '-static'"
-  ];
+      ldflags = [
+        "-s"
+        "-w"
+        "-X github.com/gruntwork-io/go-commons/version.Version=v${finalAttrs.version}"
+        "-extldflags '-static'"
+      ];
 
-  nativeInstallCheckInputs = [
-    versionCheckHook
-  ];
-  versionCheckProgramArg = "--version";
-  doInstallCheck = true;
+      doInstallCheck = true;
 
-  meta = {
-    homepage = "https://terragrunt.gruntwork.io";
-    changelog = "https://github.com/gruntwork-io/terragrunt/releases/tag/v${finalAttrs.version}";
-    description = "Thin wrapper for Terraform that supports locking for Terraform state and enforces best practices";
-    mainProgram = "terragrunt";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [
-      jk
-      qjoly
-      kashw2
-    ];
-  };
-})
+      meta = with lib; {
+        homepage = "https://terragrunt.gruntwork.io";
+        changelog = "https://github.com/gruntwork-io/terragrunt/releases/tag/v${finalAttrs.version}";
+        description = "Thin wrapper for Terraform that supports locking for Terraform state and enforces best practices";
+        mainProgram = "terragrunt";
+        license = licenses.mit;
+        maintainers = with maintainers; [
+          jk
+          qjoly
+          kashw2
+        ];
+      };
+    })
